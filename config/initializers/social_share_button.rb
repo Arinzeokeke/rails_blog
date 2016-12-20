5e288d61ea98f144c08e6a4415d9ed23440566a8
Okeke Arinze <trent9002@gmail.com>
2016-12-20 18:48:17 +0100

    more style changes. change vack 9img

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus tumblr pinterest email reddit)
+  config.allow_sites = %w(twitter facebook google_plus tumblr  email reddit)
 end
